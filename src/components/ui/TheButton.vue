ea34ac88e25790e13ce6d2087f95a9425a005a13
Nikita Novikov <n.novikov@rudesktop.ru>
2024-05-20 18:23:10 +0300

    hotfix[imports]: removed defineProps and defineEmits imports in button component

--- src/components/ui/TheButton.vue
+++ src/components/ui/TheButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineEmits, defineProps, ref, withDefaults } from "vue";
+import { ref, withDefaults } from "vue";
 
 export interface Props {
     type?: "button" | "submit" | "reset";
